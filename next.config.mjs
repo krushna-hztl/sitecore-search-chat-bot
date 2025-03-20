6ee7b80585010a57d382d4c95fc9c5c8162439a3
krushna <krushna.patel@horizontal.com>
2025-03-20 23:44:33 +0530

    Search bot added

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    SEARCH_ENV: process.env.SEARCH_ENV,
+    SEARCH_CUSTOMER_KEY: process.env.SEARCH_CUSTOMER_KEY,
+    SEARCH_API_KEY: process.env.SEARCH_API_KEY,
+    SEARCH_SOURCE: process.env.SEARCH_SOURCE,
+    SEARCH_DOMAIN_ID: process.env.SEARCH_DOMAIN_ID,
+  },
 };
 
 export default nextConfig;
